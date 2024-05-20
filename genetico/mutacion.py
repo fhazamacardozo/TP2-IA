2cf91813969c1e99fe97d82914a720920c7b49a0
Federico Hazama <fehc97@hotmail.com>
2024-05-20 19:32:05 -0300

    mutacion mas legible

--- genetico/mutacion.py
+++ genetico/mutacion.py
@@ -7,12 +7,19 @@ def mutar_cromosoma(cromosoma, probabilidad_mutacion):
 
     for i in range(len(nuevo_cromosoma)):
         if random.random() < probabilidad_mutacion:
-            segmentos_validos = [
-                seg for seg in segmentos_disponibles
-                if seg not in nuevo_cromosoma and (seg[0] == nuevo_cromosoma[i-1][1] or seg[1] == nuevo_cromosoma[(i+1) % len(nuevo_cromosoma)][0])
-            ]
+            segmento_anterior = nuevo_cromosoma[i-1][1] if i > 0 else None
+            segmento_siguiente = nuevo_cromosoma[(i+1) % len(nuevo_cromosoma)][0]
+
+            segmentos_validos = []
+            for seg in segmentos_disponibles:
+                if seg not in nuevo_cromosoma:
+                    conecta_anterior = seg[0] == segmento_anterior
+                    conecta_siguiente = seg[1] == segmento_siguiente
+                    if conecta_anterior or conecta_siguiente:
+                        segmentos_validos.append(seg)
+
             if segmentos_validos:
                 nuevo_segmento = random.choice(segmentos_validos)
                 nuevo_cromosoma[i] = nuevo_segmento
-    
+
     return nuevo_cromosoma
